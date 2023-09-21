c3a7309d74435496a1e4042047dfd8fac01ccb54
okeurizka <okeurizkaa22@gmail.com>
2023-09-21 19:09:08 +0700

    first commit

---
+++ Bola/Bola.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+int main()
+{
+	float r1, r2, r3, volume_bola;
+	cout<<"masukan nilai r1: ";
+	cin>> r1;
+	cout<<"masukan nilai r2: ";
+	cin>> r2;
+	cout<<"masukan nilai r3: ";
+	cin>> r3;
+	volume_bola=3.14*r1*r2*r3*4/3 ;
+	cout<<"nilai volume_bola adalah: "<<volume_bola<<endl;
+}
